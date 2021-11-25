e93d03cad68706621915a623994389588d1da32c
Tiago Dall'Igna <tiago@dalligna.com>
2021-11-25 14:40:04 +0100

    connection string

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,6 @@ sudo docker build -t pitaco . --rm
 sudo docker run -p 7000:7000 -t \
   --env ASPNETCORE_ENVIRONMENT=Production \
   --env ASPNETCORE_URLS=http://+:6000 \
-  --env CONNECTION_STRING=Data Source=./pitaco.db;Pooling=True \
+  --env CONNECTION_STRING="Data Source=./pitaco.db;Pooling=True" \
   --name pitaco -d --restart unless-stopped -it pitaco
 cd ..
